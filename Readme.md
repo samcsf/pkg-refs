9d70d518e4a3086f2da6cd3ecff49fa5e7ab7735
samcsf <samfc@me.com>
2018-06-05 01:26:44 +0800

    Add parse-mode & path-mode for parsing local package.json

--- Readme.md
+++ Readme.md
@@ -9,7 +9,19 @@ npm install -g pkg-refs
 ## Usage
 Command line tool with short-form pkgd. After a short time, it will list out all the dependecies and the decriptions for them.
 ```shell
-pkgd <package>
+# search package name online
+pkgd <package name>
+
+# parse local package.json
+pkgd < package.json
+package.json | pkgd
+
+# or give the path to package.json
+pkgd anyProject/package.json
+
+# even shorter, cd to project folder,
+# and read the package.json automatically
+pkgd .
 ```
 
 ## Example
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 #! /usr/bin/env node
-
+const fs = require('fs');
+const path = require('path');
 const shell = require('shelljs');
 const chalk = require('chalk');
 shell.config.silent = true;
+
+// retrieve stream from command when no args
+let inputBuffer = '';
+if (!process.argv[2]) {
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', chunk => inputBuffer += chunk);
+  process.stdin.on('end', () => start());
+} else {
+  // else directly start
+  start();
+}
 
 function execute(cmd, callback) {
   if (callback && typeof callback === 'function') {
@@ -39,13 +51,61 @@ function printResults({result, cmd}){
   }
 }
 
-function start() {
-  let target = process.argv[2];
-  fetchList(target).then(packages => {
-    console.log(`----------- Dependencies for ${chalk.green(target)}----------------`)
-    let cmds = packages.map(pkg => `npm show ${pkg} description`);
-    cmds.forEach(cmd => execute(cmd).then(printResults));
-  });
+function fetchDesc(package, dependencies) {
+  console.log(`----------- Dependencies for ${chalk.green(package)}----------------`)
+  let cmds = dependencies.map(dp => `npm show ${dp} description`);
+  cmds.forEach(cmd => execute(cmd).then(printResults));
 }
 
-start();
+function showUsage() {
+  console.log('Usage:');
+  console.log('        Parse package.json: cat package.json | pkgd');
+  console.log('        Directly input    : pkgd <package name>');
+}
+
+async function start() {
+  let pkg = '';
+  let mode = '';
+
+  if (inputBuffer) {
+    try {
+      pkg = JSON.parse(inputBuffer.toString());
+      mode = 'PARSE';    
+    } catch(error) {
+      console.error('Error occured when parsing target file...');
+      console.log('Usage:');
+      console.log('       cat package.json | pkgd');
+      console.log(' or    pkgd < package.json');
+      process.exit(-1);
+    }
+  } else {
+    pkg = process.argv[2];
+    let query = path.normalize(pkg);
+    let testPath = path.join(query, 'package.json');
+    console.log(`Looking for package.json in ${query}`);
+    if (fs.existsSync(testPath)) {
+      pkg = JSON.parse(fs.readFileSync(testPath, {encoding: 'utf8'}).toString());
+      mode = 'PATH';
+    } else {
+      console.log(`Package config not found locally, try to fetch online...`);
+      mode = 'FETCH';
+    }
+  }
+
+  if (!pkg) {
+    console.error('Invalid package name.');
+    showUsage();
+  }
+
+  switch (mode) {
+    case 'FETCH':
+      fetchDesc(pkg, await fetchList(pkg));
+      break;
+    case 'PARSE':
+    case 'PATH':
+      fetchDesc(pkg.name, Object.keys(pkg.dependencies));
+      break; 
+  }
+}
+
+// start();
